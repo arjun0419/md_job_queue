bf38e6894ebbb5f5911dcec10ee894c0da769adb
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-05-15 19:50:43 -0700

    added POST and GET routes

--- server/app.js
+++ server/app.js
@@ -11,4 +11,17 @@ app.get('/', (req, res) => {
   res.send();
 });
 
+app.get('/api/:jobID/status', (req, res) => {
+  const { jobID } = req.params;
+  console.log(`get request received in server for Job ID ${jobID}`);
+  res.send(`hello from server, we got your request for job ID ${jobID}`);
+});
+
+app.post('/api/:url/post', (req, res) => {
+  const { url } = req.params;
+  console.log(`post request received in server for Job ID ${url}`);
+  res.send(`hello from server, we got your request for URL ${url}. We will cache this html shortly. Please come back soon and check JOB ID 019`);
+});
+
 module.exports = app;
+
